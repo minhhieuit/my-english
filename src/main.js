f2079732656521ed0a04a5efbbca39e5589ca2f8
Jacky Nguyen <cutii@DESKTOP-B3LCIQI>
2023-05-31 17:16:48 +0700

    update config routes

--- src/main.js
+++ src/main.js
@@ -6,15 +6,16 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// add routes
+const routes = require('./resources/routes/index')
+routes(app)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(morgan('combined'))
 app.engine('hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'))
-app.get('/', (req, res) => {
-    res.render('home')
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
